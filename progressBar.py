7f709f40cef0c2cf9432cdbec92de39ac626bb43
Dvip <98635463+dvip1@users.noreply.github.com>
2022-07-23 12:52:56 +0530

    Update progressBar.py

--- progressBar.py
+++ progressBar.py
@@ -4,7 +4,7 @@ def progress_bar(progress, total):
 
   percent = 100 * (progress/float(total))
 
-  bar = '|' * int(percent) + '-' * (100-int(perce>
+  bar = '|' * int(percent) + '-' * (100-int(percent))
 
   print(f"\r[{bar}] {percent:.2f} %", end="\r")
 
